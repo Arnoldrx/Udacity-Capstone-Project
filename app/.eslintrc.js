a3cdb4182a50f64a553c88b20450fe668e939656
arnoldrx <tagnearnold2080@gmail.com>
2022-04-13 20:40:50 +0100

    fixes

--- app/.eslintrc.js
+++ app/.eslintrc.js
@@ -1,13 +1,26 @@
 module.exports = {
-    "env": {
-        "node": true,
-        "commonjs": true,
-        "es6": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": 6
-    },
-    "rules": {
+  "env": {
+    "node": true,
+    "commonjs": true,
+    "es2021": true,
+    "browser": true
+  },
+  "extends": [
+      "eslint:recommended",
+      "plugin:react/recommended",
+      "plugin:jsx-a11y/recommended"
+  ],
+  "plugins": ["react", "jsx-a11y"],
+  "parserOptions": {
+    "ecmaVersion": 12,
+    "sourceType": "module",
+    "ecmaFeatures": {
+      "jsx": true
     }
+  },
+  "rules": {
+    "react/prop-types": 0,
+    "indent": ["error", 2],
+    "linebreak-style": 1,
+  }
 };
